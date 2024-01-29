fcd4592b75883dab24c3b4d5cad4dbd62a9b24d2
lizard-heart <hgustafson@protonmail.com>
2024-01-29 13:32:36 -0800

    fix dimensions

--- Polodo/Lessons.swift
+++ Polodo/Lessons.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct Lessons: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack() {
+            Text("Back")
+                .font(Font.custom("Poppins", size: 25).weight(.medium))
+                .foregroundColor(.white)
+                .offset(x: -130, y: -353)
+            Text("Start your daily lesson!")
+                .font(Font.custom("Poppins", size: 25).weight(.medium))
+                .foregroundColor(.white)
+                .offset(x: 0, y: -187)
+            Text("Global")
+                .font(Font.custom("Poppins", size: 25).weight(.medium))
+                .foregroundColor(.white)
+                .offset(x: 0, y: -22)
+            Text("Personal")
+                .font(Font.custom("Poppins", size: 25).weight(.medium))
+                .foregroundColor(.white)
+                .offset(x: 0, y: 80.50)
+            Text("Daily Article")
+                .font(Font.custom("Poppins", size: 25).weight(.medium))
+                .foregroundColor(.white)
+                .offset(x: 0, y: 204.50)
+        }
+        .frame(width: 395, height: 844)
+        .background(Color(red: 0.13, green: 0.16, blue: 0.22))
     }
 }
 

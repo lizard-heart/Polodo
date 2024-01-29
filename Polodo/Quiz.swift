553ee7596e40a74a56dbb02dda9bde58cf6a5eb7
lizard-heart <hgustafson@protonmail.com>
2024-01-29 13:40:48 -0800

    add quiz page

---
+++ Polodo/Quiz.swift
@@ -0,0 +1,18 @@
+//
+//  Quiz.swift
+//  Polodo
+//
+//  Created by Henry Gustafson on 1/29/24.
+//
+
+import SwiftUI
+
+struct Quiz: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Quiz()
+}

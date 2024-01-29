03ecc0fc7bb949364c4e4de3db5d8b5f812ac947
lizard-heart <hgustafson@protonmail.com>
2024-01-29 13:22:18 -0800

    added lessons view controller

---
+++ Polodo/Lessons.swift
@@ -0,0 +1,18 @@
+//
+//  Lessons.swift
+//  Polodo
+//
+//  Created by Henry Gustafson on 1/29/24.
+//
+
+import SwiftUI
+
+struct Lessons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Lessons()
+}
